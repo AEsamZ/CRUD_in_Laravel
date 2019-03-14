59b6633f368bef3d57eb6581314009235f8f7dd6
AEsamZ <ahmed.esam5996@gmail.com>
2019-03-14 13:23:46 +0200

    auth system

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/posts', 'PostsController@index')->name('posts.index');
+    Route::get('/posts/create','PostsController@create')->name('posts.create');
+    Route::post('/posts','PostsController@store')->name('posts.store');
+    Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
+    Route::get('/posts/{post}','PostsController@show')->name('posts.show');
+    Route::delete('/posts/{post}','PostsController@destroy')->name('posts.destroy');
+    Route::put('/posts/{post}','PostsController@update')->name('posts.update');
+});
 
-Route::get('/posts', 'PostsController@index')->name('posts.index');
-Route::get('/posts/create','PostsController@create')->name('posts.create');
-Route::post('/posts','PostsController@store')->name('posts.store');
-Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
-Route::get('/posts/{post}','PostsController@show')->name('posts.show');
-Route::delete('/posts/{post}','PostsController@destroy')->name('posts.destroy');
-Route::put('/posts/{post}','PostsController@update')->name('posts.update');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
